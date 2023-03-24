e8b901ad8b4b385c60ee0d59357228121e47d444
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-03-24 12:25:36 -0500

    init pkg

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,23 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const inq = require('inquirer')
+const file = require('fs')
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// console.log(inq)
+// console.log(file)
 
-// Function call to initialize app
-init();
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();

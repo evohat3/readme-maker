42d6803a8d4446487d53813bd18fb5bdebdea75d
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-03-24 00:37:24 -0500

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+const inq = require('inquirer')
+
+const file = require('fs')
+
+
+// console.log(inq)
+// console.log(file)
+
+// // TODO: Create an array of questions for user input
+const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();

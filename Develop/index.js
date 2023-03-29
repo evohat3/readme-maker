10645805b6b8637c20c4f20e1774c6c42ade52dd
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-03-29 06:06:31 -0500

    update script

--- Develop/index.js
+++ Develop/index.js
@@ -2,158 +2,150 @@
 
 const inq = require('inquirer')
 const file = require('fs')
-const output = [];
-
+const generateMarkdown = require('./utils/generateMarkdown')
 
 
 // console.log(inq)
 // console.log(file)
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-inq.prompt ([{
-    type: 'input',
-    name: 'title',
-    message: 'Enter the title to your readme: ',
-    default: 'README'
-},
-{
-    type: 'input',
-    name: 'desc1',
-    message: 'What was your motivation for this project? ',
-},
-{
-    type: 'input',
-    name: 'desc2',
-    message: 'why did you build this project? ',
-},
-{
-    type: 'input',
-    name: 'desc3',
-    message: 'what problem does your project solve? ',
-},
-{
-    type: 'input',
-    name: 'desc4',
-    message: 'what did you learn in the building process? ',
-},
-{
-    type: 'confirm',
-    name: 'install',
-    message: 'to enter the steps it takes to install (press enter to start) ',
-    default: true,
-},
-{
-    type: 'input',
-    name: 'insStep',
-    message: "(enter step)",
-},
-{
-    type: 'confirm',
-    name: 'askAgain',
-    message: 'Want to another step? (just hit enter for YES)?',
-    default: true,
- },
-
-])
-.then((an) => {
-
-
-// var readMe = ` 
-// # <${an.title}>
-
-// ## Description
-
-// What was the motivation for building this project?
-// - ${an.desc1}
-
-// why did you build this project?
-// - ${an.desc2}
-
-// what problem does your project solve?
-// - ${an.desc3}
-
-// what did you learn in the building process?
-// - ${an.desc4}
-
-// ## Table of Contents 
-
-// - [Installation](#installation)
-// - [Usage](#usage)
-// - [Credits](#credits)
-// - [License](#license)
-
-// ## Installation
-
-// - Step 1: ${an.ins1}
-
-// - Step 2: ${an.ins2}
-
-// - Step 3: ${an.ins3}
-
-// ## Usage
-
-// ## Credits
-
-// List your collaborators, if any, with links to their GitHub profiles.
-
-// If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-// If you followed tutorials, include links to those here as well.
-
-// ## License
-
-// The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
-// ---
-
-// 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-// ## Badges
-
-// ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-// Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-// ## Features
-
-// If your project has a lot of features, list them here.
-
-// ## How to Contribute
-
-// If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-// ## Tests
-
-// Go the extra mile and write tests for your application. Then provide examples on how to run them here.
-
-// `;
-
-// console.log(readMe)
+// console.log(generateMarkdown)
 
 
 
+// question prompts
+ const quest = [{
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title to your readme: ',
+        default: 'README'
+    },
+    {
+        type: 'input',
+        name: 'desc1',
+        message: 'What was your motivation for this project? ',
+    },
+    {
+        type: 'input',
+        name: 'desc2',
+        message: 'why did you build this project? ',
+    },
+    {
+        type: 'input',
+        name: 'desc3',
+        message: 'what problem does your project solve? ',
+    },
+    {
+        type: 'input',
+        name: 'desc4',
+        message: 'what did you learn in the building process? ',
+    },
+    {
+        type: 'confirm',
+        name: 'install',
+        message: '(to enter the steps it takes to install press enter)'
+    },
+    {
+        type: 'input',
+        name: 'ins1',
+        message: 'step 1'
+    },
+    {
+        type: 'input',
+        name: 'ins2',
+        message: 'step 2'
+    },
+    {
+        type: 'input',
+        name: 'ins3',
+        message: 'step 3'
+    },
+    {
+        type: 'input',
+        name: 'use',
+        message: 'provide instructions on how to use: '
+    },
+    {
+        type: 'input',
+        name: 'useEx',
+        message: 'give an example of usage'
+    },
+    {
+        type: 'input',
+        name: 'useimg',
+        message: 'provide link or filepath to screenshot of example'
+    },
+    {
+        type: 'input',
+        name: 'creditscol',
+        message: 'list your collaborators (credits)'
+    },
+    {
+        type: 'input',
+        name: 'creditstec',
+        message: 'list your tech (credits)'
+    },
+    {
+        type: 'input',
+        name: 'badge',
+        message: 'enter your badge link'
+    },
+    {
+        type: 'input',
+        name: 'feature1',
+        message: 'enter feature 1'
+
+    },
+    {
+        type: 'input',
+        name: 'feature2',
+        message: 'enter feature 2'
+
+    },
+    {
+        type: 'input',
+        name: 'feature3',
+        message: 'enter feature 3'
+
+    },
+
+    {
+        type: 'input',
+        name: 'contrib',
+        message: 'how can others contribute?'
+
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'how do we test this code?'
+
+    },
+
+    {
+            type: 'list',
+            name: 'license',
+            message: 'What License would you like to use?',
+            choices: ['Apache 2.0', 'MIT', 'GNU General Public 3.0', 'NONE']
+            
+    },
+];
+
+
+// Init Function
+ function init() {
+    inq.prompt(quest).then((an) => {
+
+        file.writeFile('README.md', generateMarkdown(an), (err) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log('File saved successfully!');
+            }
+          });
+    })
+ }
+
+// Function call to initialize app
+init();
 
 
 
-
-
-
-
-
-})
-.catch((err) => {
-    console.error(err);
-});
-
-
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,178 @@
+const license = require('./licenseInfo')
+
+
+
+// console.log(license.apache)
+
+// console.log (license.MIT)
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(an) {
+    if (an.license == 'Apache 2.0')
+    return `${license.apachbadge}`
+    
+    if (an.license == 'MIT')
+    return `${license.mitbadge}`
+    
+    if (an.license == 'GNU General Public 3.0')
+    return `${license.gnubadge}`
+
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(an) {
+    if (an.license == 'Apache 2.0')
+    return `${license.apachlink}`
+    
+    if (an.license == 'MIT')
+    return `${license.mitlink}`
+    
+    if (an.license == 'GNU General Public 3.0')
+    return `${license.gnulink}`
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+
+function renderLicenseSection(an) {
+if (an.license == 'Apache 2.0')
+return `${license.apache}`;
+
+if (an.license == 'MIT')
+return `${license.MIT}`;
+
+if (an.license == 'GNU General Public 3.0')
+return `${license.GNU}`;
+
+
+if(an.license == 'NONE')
+return `${license.none}`;
+
+} 
+
+
+
+//  Create a function to generate markdown for README
+function generateMarkdown(an) {
+  return ` 
+  # ${an.title}
+  ---
+  ---
+  ## Table of Contents 
+      
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [Badges](#badges)
+  * [Features](#features)
+  * [How to Contribute](#how-to-contribute)
+  * [Tests](#tests)
+  * [License](#license)
+      
+    
+
+  ![alt text](${renderLicenseBadge(an)})   
+ [${an.license} LINK](${renderLicenseLink(an)})
+
+
+  ---
+  ---
+  ## Description
+      
+          What was the motivation for building this project?
+          - ${an.desc1}
+      
+      
+          why did you build this project?
+          - ${an.desc2}
+      
+      
+          what problem does your project solve?
+          - ${an.desc3}
+      
+      
+          what did you learn in the building process?
+          - ${an.desc4}
+      
+  ---
+  ---
+      
+  ## Installation
+      
+  
+      How to install
+  
+      - Step 1: ${an.ins1}
+      
+      - Step 2: ${an.ins2}
+      
+      - Step 3: ${an.ins3}
+      
+      
+  ---
+  ---
+  ## Usage
+      
+      How to use: 
+      
+      - ![IMG](files://${an.useimg}) 
+      
+      
+      
+      - ${an.use}
+      
+      - ${an.useEx}
+      
+  ---
+  ---
+  ## Credits
+      
+      Collaborators - ${an.creditscol}
+      
+      Tech - ${an.creditstec}
+      
+  ---
+  ---
+      
+  ## Badges
+  ![alt text](${an.badge})     
+      
+  ---
+  ---
+  ## Features
+ 
+ * ${an.feature1}
+  
+ * ${an.feature2}
+  
+ * ${an.feature3}
+
+  ---
+  ---
+  ## How to Contribute
+      
+ *  ${an.contrib}     
+  ---
+  ---
+  ## Tests
+* ${an.test}
+  
+  ---
+  ---   
+  ## License 
+      
+
+
+    ${renderLicenseSection(an)}
+
 
 `;
 }
 
 module.exports = generateMarkdown;
-
-
-
